2b417a558ccf5132fe6e80cfdfee99f1dc0ee8aa
Atharva <96356279+AtharvaPote@users.noreply.github.com>
2022-03-25 18:43:30 +0530

    4

--- DB/db.js
+++ DB/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'sakec',
-  password: 'sakec123',
+  user: 'root',
+  password: 'root',
   database: 'mini-v1'
 });
 
